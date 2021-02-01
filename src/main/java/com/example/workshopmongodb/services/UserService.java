eae1a51ec7d5eb5cad16aa0d2a77b69ca2f7ad16
andreiguido <andrei.guido@outlook.com>
2021-02-01 14:59:23 -0300

    User insertion with POST

--- src/main/java/com/example/workshopmongodb/services/UserService.java
+++ src/main/java/com/example/workshopmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.workshopmongodb.domain.User;
+import com.example.workshopmongodb.dto.UserDTO;
 import com.example.workshopmongodb.repository.UserRepository;
 import com.example.workshopmongodb.services.exception.ObjectNotFoundException;
 
@@ -15,13 +16,21 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
-		Optional<User> obj= repo.findById(id);
+		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
-		}
 	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+}

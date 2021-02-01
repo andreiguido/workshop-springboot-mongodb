c907db5a1fda524cfa3fc92184cee748c208e680
andreiguido <andrei.guido@outlook.com>
2021-02-01 18:41:08 -0300

    Simple query with @Query

--- src/main/java/com/example/workshopmongodb/services/PostService.java
+++ src/main/java/com/example/workshopmongodb/services/PostService.java
@@ -15,14 +15,13 @@ public class PostService {
 
 	@Autowired
 	private PostRepository repo;
-
-
+	
 	public Post findById(String id) {
 		Optional<Post> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
-	public List<Post> findByTitle(String text){
-		return repo.findByTitleContainingIgnoreCase(text);
+	public List<Post> findByTitle(String text) {
+		return repo.searchTitle(text);
 	}
 }
